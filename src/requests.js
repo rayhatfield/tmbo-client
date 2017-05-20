2ef931642d4a3a9138f7af2067e437047131fdf8
Ray Hatfield <ray.hatfield@nextthought.com>
2017-05-19 20:47:38 -0500

    get isn't an arrow function; add post method

--- src/requests.js
+++ src/requests.js
@@ -1,12 +1,34 @@
 import {stringify} from 'query-string';
 
-export const get = async (url, params = {}) => {
-	const query = queryString.stringify(params);
-	const result = await fetch(`${url}?${query}`)
+export async function get (url, params = {}) {
+	const query = stringify(params);
+	const result = await fetch(`${url}?${query}`);
 	if (result.ok) {
 		const json = await result.json();
 		console.log(json);
 		return json;
 	}
 	throw new Error;
-};
+}
+
+export async function post (url, params = {}) {
+	const result = await fetch(url, {
+		method: 'POST',
+		// body: JSON.stringify(params)
+		body: asFormData(params)
+	});
+
+	if (result.ok) {
+		const json = await result.json();
+		console.log(json);
+		return json;
+	}
+}
+
+function asFormData (data) {
+	const f = new FormData();
+	for (const key of Object.keys(data)) {
+		f.append(key, data[key]);
+	}
+	return f;
+}
